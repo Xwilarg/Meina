f9ca78a9d600794bf54936503c507b6fb7b2c61b
Xwilarg <xwilarg@yahoo.fr>
2020-04-11 22:57:21 +0200

    Join red team on connection

--- Meina/Program.cs
+++ Meina/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Net.Sockets;
 using TerrariaBot;
 
@@ -6,29 +7,30 @@ namespace Meina
 {
     class Program
     {
-        static void Exit()
-        {
-            Console.WriteLine("Press any key to continue");
-            Console.ReadKey();
-            Environment.Exit(1);
-        }
-
+        private static TerrariaClient client;
         static void Main(string[] args)
         {
-            TerrariaClient client;
             try
             {
-                client = new TerrariaClient("localhost", new PlayerInformation("Meina", 8, new Color(255, 248, 220), new Color(255, 255, 153), new Color(0, 0, 255),
-                    new Color(255, 0, 0), new Color(127, 0, 0), new Color(255, 248, 220), new Color(0, 0, 0), PlayerDifficulty.Easy));
+                client = new TerrariaClient("localhost", new PlayerInformation("Meina", 11, new Color(255, 248, 220), new Color(255, 255, 153), new Color(0, 0, 255),
+                    new Color(255, 0, 0), new Color(127, 0, 0), new Color(255, 248, 220), new Color(0, 0, 0), PlayerDifficulty.Easy), File.Exists("password.txt") ? File.ReadAllText("password.txt") : "");
             }
             catch (SocketException se)
             {
                 Console.WriteLine("Can't connect to server: " + se.Message + Environment.NewLine + "Make sure that your Terraria server is online.");
-                Exit();
+                Console.WriteLine("Press any key to continue");
+                Console.ReadKey();
+                return;
             }
+            client.ServerJoined += Ai;
             Console.WriteLine("Bot is connected");
             while (true)
             { }
+        }
+
+        private static void Ai()
+        {
+            client.JoinTeam(Team.Red);
         }
     }
 }
