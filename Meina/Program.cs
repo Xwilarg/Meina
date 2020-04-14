54ed91ce09099bded3c420c0a462013bac9cb8c7
Xwilarg <xwilarg@yahoo.fr>
2020-04-15 00:36:24 +0200

    Improve bot

--- Meina/Program.cs
+++ Meina/Program.cs
@@ -3,21 +3,23 @@ using System.IO;
 using System.Net.Sockets;
 using System.Threading;
 using TerrariaBot;
+using TerrariaBot.Client;
 using TerrariaBot.Entity;
+using TerrariaBot.Steam.Client;
 
 namespace Meina
 {
     class Program
     {
-        private static TerrariaClient client;
+        private static IPClient client;
         private static AutoResetEvent autoEvent = new AutoResetEvent(false);
         static void Main(string[] _)
         {
             try
             {
-                client = new TerrariaClient(LogLevel.Debug);
+                client = new IPClient(LogLevel.Debug);
                 client.ServerJoined += Ai;
-                client.Connect("localhost", new PlayerInformation("Meina", 11, new Color(255, 248, 220), new Color(245, 245, 220), new Color(0, 0, 255),
+                client.ConnectWithIP("localhost", new PlayerInformation("Meina", 11, new Color(255, 248, 220), new Color(245, 245, 220), new Color(0, 0, 255),
                     new Color(255, 0, 0), new Color(127, 0, 0), new Color(255, 248, 220), new Color(0, 0, 0), PlayerDifficulty.Easy), File.Exists("password.txt") ? File.ReadAllText("password.txt") : "");
             }
             catch (SocketException se)
