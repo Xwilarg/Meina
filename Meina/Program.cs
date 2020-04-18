d437fb8100fe32706754b7fc04910bae66e4b70d
Xwilarg <xwilarg@yahoo.fr>
2020-04-18 16:21:20 +0200

    Add log event

--- Meina/Program.cs
+++ Meina/Program.cs
@@ -17,8 +17,9 @@ namespace Meina
         {
             try
             {
-                client = new IPClient(LogLevel.Debug);
+                client = new IPClient();
                 client.ServerJoined += Ai;
+                client.Log += Log;
                 client.ConnectWithIP(File.ReadAllText("ip.txt"), new PlayerInformation("Meina", 11, new Color(255, 248, 220), new Color(245, 245, 220), new Color(0, 0, 255),
                     new Color(255, 0, 0), new Color(127, 0, 0), new Color(255, 248, 220), new Color(0, 0, 0), PlayerDifficulty.Easy), File.Exists("password.txt") ? File.ReadAllText("password.txt") : "");
             }
@@ -37,6 +38,31 @@ namespace Meina
             me.SendChatMessage("I'm connected!");
             me.JoinTeam(Team.Red);
             me.TogglePVP(true);
+        }
+
+        private static void Log(LogLevel logLevel, string message)
+        {
+            var color = Console.ForegroundColor;
+            switch (logLevel)
+            {
+                case LogLevel.Error:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    break;
+
+                case LogLevel.Warning:
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    break;
+
+                case LogLevel.Info:
+                    Console.ForegroundColor = ConsoleColor.White;
+                    break;
+
+                case LogLevel.Debug:
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                    break;
+            }
+            Console.WriteLine(message);
+            Console.ForegroundColor = color;
         }
     }
 }
