f9f508981192c4fa1c3659b9ddb532f1a620ed12
Xwilarg <xwilarg@yahoo.fr>
2020-04-23 01:55:00 +0200

    Add commands

--- Meina/Program.cs
+++ Meina/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Sockets;
 using System.Threading;
 using TerrariaBot;
@@ -11,17 +12,51 @@ namespace Meina
 {
     class Program
     {
-        private static IPClient client;
-        private static AutoResetEvent autoEvent = new AutoResetEvent(false);
+        private AClient client;
+        private readonly AutoResetEvent autoEvent = new AutoResetEvent(false);
+        private readonly PlayerInformation myInfos = new PlayerInformation("Meina", 11, new Color(255, 248, 220), new Color(245, 245, 220), new Color(0, 0, 255),
+            new Color(255, 0, 0), new Color(127, 0, 0), new Color(255, 248, 220), new Color(0, 0, 0), PlayerDifficulty.Easy);
+        private Random rand = new Random();
         static void Main(string[] _)
+            => new Program().Start();
+
+        public Program()
         {
             try
             {
-                client = new IPClient();
+                Console.WriteLine("Enter your connection method:");
+                Console.WriteLine("1. IP");
+                Console.WriteLine("2. Steam");
+                char choice;
+                do
+                {
+                    choice = Console.ReadKey().KeyChar;
+                } while (choice == '1' || choice == '2');
+                string ip;
+                if (choice == '1')
+                {
+                    client = new IPClient();
+                    Console.WriteLine("Enter IP address:");
+                    ip = Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Enter Steam ID to join:");
+                    ip = Console.ReadLine();
+                }
                 client.ServerJoined += Ai;
                 client.Log += Log;
-                client.ConnectWithIP(File.ReadAllText("ip.txt"), new PlayerInformation("Meina", 11, new Color(255, 248, 220), new Color(245, 245, 220), new Color(0, 0, 255),
-                    new Color(255, 0, 0), new Color(127, 0, 0), new Color(255, 248, 220), new Color(0, 0, 0), PlayerDifficulty.Easy), File.Exists("password.txt") ? File.ReadAllText("password.txt") : "");
+                client.ChatMessageReceived += Chat;
+                Console.WriteLine("Enter password if any:");
+                string password = Console.ReadLine();
+                if (choice == '1')
+                {
+                    ((IPClient)client).ConnectWithIP(ip, myInfos, password);
+                }
+                else
+                {
+                    ((SteamClient)client).ConnectWithSteamId(ulong.Parse(ip), myInfos, password);
+                }
             }
             catch (SocketException se)
             {
@@ -30,17 +65,46 @@ namespace Meina
                 Console.ReadKey();
                 return;
             }
+        }
+
+        public void Start()
+        {
             autoEvent.WaitOne();
         }
 
-        private static void Ai(PlayerSelf me)
+        private void Chat(Player author, string message)
+        {
+            var me = client.GetPlayerSelf();
+            if (message.ToLower().StartsWith("meina"))
+            {
+                message = message.Substring(5).Trim().ToLower();
+                if (message.StartsWith("go to"))
+                {
+                    message = message.Substring(5).Trim();
+                    var player = client.GetAllPlayers().Where(x => x.GetName().ToLower() == message).FirstOrDefault();
+                    if (player == null) me.SendChatMessage("There is nobody with that name here");
+                    else
+                    {
+                        SendSuccessMessage(me);
+                        me.Teleport(player.GetPosition());
+                    }
+                }
+            }
+        }
+
+        private void SendSuccessMessage(PlayerSelf me)
+        {
+            me.SendChatMessage(okayLines[rand.Next(0, okayLines.Length)]);
+        }
+
+        private void Ai(PlayerSelf me)
         {
             me.SendChatMessage("I'm connected!");
             me.JoinTeam(Team.Red);
             me.TogglePVP(true);
         }
 
-        private static void Log(LogLevel logLevel, string message)
+        private void Log(LogLevel logLevel, string message)
         {
             var color = Console.ForegroundColor;
             switch (logLevel)
@@ -64,5 +128,10 @@ namespace Meina
             Console.WriteLine(message);
             Console.ForegroundColor = color;
         }
+
+        private string[] okayLines = new[]
+        {
+            "Sure", "Okay", "Roger", "Yup", "Right away"
+        };
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Meina
 A bot that can play at Terraria
+
+## Starting the bot
+After starting the bot, choose if you want to connect it using a direct IP or Steam.<br/>
+Provide a password if needed (just fill nothing if there is none)<br/>
+And that's all!
+
+## Commands
+You can ask the bot to do things by sending chat messages ingame.<br/>
+For now the bot will move around using teleportation.<br/>
+The command must follow this format:
+```
+Meina [command] [argument(s)]
+```
+The available commands are the following:
+ - Go to [player name]: Teleport to the location of another player
